81cb450e9c3c2c24e53c8420ad2982861b99d06e
pooranaS <spooranakala@gmail.com>
2022-04-18 18:49:34 +0530

    Add files via upload

---
+++ LearnDataTypes.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class LearnDataTypes {
+
+	public static void main(String[] args) {
+		
+		byte fuelCapacity = 50;
+		short maxSpeedLimit = 120;
+		int price = 520000;
+		long engNum = 456789056L;
+		float emiPerMonth = 12.5F;
+		double kmRan = 22.57969;
+		char variantType = 'G';
+		boolean isGasFilled = true;
+		
+	}
+
+}
